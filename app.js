80d2a76869bd29c1e4f1cc49ddea1b002cabb8a5
varsha-sera <vsera0927@gmail.com>
2022-03-29 16:26:30 +0530

    jgf

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const port = process.env.PORT || 3000
 mongoose.connect("mongodb+srv://vsera0927:varsha123@database.afv8a.mongodb.net/firstapidb?retryWrites=true&w=majority")
 const bodyparser = require('body-parser');
 const userRouter = require('./route/user.route');
 const app = express();
 app.use(bodyparser.urlencoded({extended:true}));
 app.use("/user",userRouter);
-app.listen(3000,()=>{console.log("Server Stated")});
+app.listen(port,()=>{console.log("Server Stated")});
