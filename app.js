70511de6d8bd0b0db132b13dbb69a133dd3a745d
varsha-sera <vsera0927@gmail.com>
2022-03-30 11:42:08 +0530

    cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const port = process.env.PORT || 3000
 mongoose.connect("mongodb+srv://vsera0927:varsha123@database.afv8a.mongodb.net/firstapidb?retryWrites=true&w=majority")
 const bodyparser = require('body-parser');
 const userRouter = require('./route/user.route');
 const app = express();
+app.use(cors());
 app.use(bodyparser.urlencoded({extended:true}));
 app.use("/user",userRouter);
 app.listen(port,()=>{console.log("Server Stated")});
